59fb03fc6705ad72eb69bf6ec412612209cb5b81
rossmoody <fiftyfivehis@gmail.com>
2022-05-11 21:13:05 -0700

    Initial Commit

---
+++ Assembl/AssemblApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssemblApp.swift
+//  Assembl
+//
+//  Created by Ross Moody on 5/11/22.
+//
+
+import SwiftUI
+
+@main
+struct AssemblApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assembl/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Assembl
+//
+//  Created by Ross Moody on 5/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
