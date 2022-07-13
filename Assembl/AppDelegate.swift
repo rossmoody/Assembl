f173404784cb738ea8b7941382a2fccac6ecf9ce
rossmoody <fiftyfivehis@gmail.com>
2022-07-13 12:13:47 -0700

    1.2.1

--- Assembl/AppDelegate.swift
+++ Assembl/AppDelegate.swift
@@ -5,6 +5,7 @@ import SwiftUIWindow
 class AppDelegate: NSObject, NSApplicationDelegate {
     lazy var statusItem = with(NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)) {
         $0.button?.image = NSImage(named: "MenuIcon")
+        $0.button?.image?.isTemplate = true
         $0.button?.toolTip = loc("ASSEMBL", "App name")
         $0.menu = StatusMenu(title: loc("ASSEMBL", "App name"))
     }
